dfbd832200c7755431d669a473d79c3954469fec
4b-n4v <ab.navilon@gmail.com>
2024-05-25 12:43:44 -0500

    Finished code for scanning number literals

--- Scanner.java
+++ Scanner.java
@@ -85,6 +85,7 @@ class Scanner {
 					addToken(SLASH);
 				}
 				break;
+			// Whitespace characters
 			case ' ':
 			case '\r':
 			case '\t':
@@ -108,8 +109,23 @@ class Scanner {
 		}
 	}
 
-	// HELPER: Looks for characters encased in " ".
+	// HELPER: Looks for integers and floating point numbers
+	private void number() {
+		while (isDigit(peek()))
+			advance();
+		// Look a fractional part.
+		if (peek() == '.' && isDigit(peekNext())) {
+			// Consume the "."
+			advance();
 
+			while (isDigit(peek()))
+				advance();
+		}
+
+		addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+	}
+
+	// HELPER: Looks for characters encased in " ".
 	private void string() {
 		while (peek() != '"' && !isAtEnd()) { // While the character is not an end quation nor at the end of the source
 			if (peek() == '\n') // If character is a new line, then increment line
@@ -136,6 +152,14 @@ class Scanner {
 			return '\0';
 
 		return source.charAt(current);
+	}
+
+	// HELPER: Looks at the next character but does not consume it as a lexeme.
+	private char peekNext() {
+		if (current + 1 >= source.length())
+			return '\0';
+
+		return source.charAt(current + 1);
 	}
 
 	// HELPER: check if character is a digit.
