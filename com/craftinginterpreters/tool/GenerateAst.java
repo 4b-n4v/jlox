3785c5591da57d11c5fecf098dba22bd240568a7
4b-n4v <ab.navilon@gmail.com>
2024-06-11 19:07:05 -0500

    Fixed formatting

--- com/craftinginterpreters/tool/GenerateAst.java
+++ com/craftinginterpreters/tool/GenerateAst.java
@@ -83,7 +83,7 @@ public class GenerateAst {
 		writer.println();
 		writer.println("	@Override");
 		writer.println("	<R> R accept(Visitor<R> visitor) {");
-		writer.println("	return visitor.visit" +
+		writer.println("		return visitor.visit" +
 				className + baseName + "(this);");
 		writer.println("	}");
 
@@ -93,7 +93,7 @@ public class GenerateAst {
 			writer.println("	final " + field + ";");
 		}
 
-		writer.println(" }");
+		writer.println("}");
 	}
 
 }
