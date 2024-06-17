e0a8aabcf2973d0f682b12205a7f979f93a89cdf
4b-n4v <ab.navilon@gmail.com>
2024-06-16 21:57:00 -0500

    Finished precedence level of functions for parsing

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -64,6 +64,35 @@ class Parser {
 		return expr;
 	}
 
+	private Expr unary() {
+		if (match(BANG, MINUS)) {
+			Token operator = previous();
+			Epxr right = unary();
+			return new Expr.Unary(operator, right);
+		}
+
+		return primary();
+	}
+
+	private Expr primary() {
+		if (match(FALSE))
+			return new Expr.literal(false);
+		if (match(TRUE))
+			return new Expr.literal(true);
+		if (match(NIL))
+			return new Expr.literal(null);
+
+		if (match(NUMBER, STRING)) {
+			return new Expr.Literal(previous().literal);
+		}
+
+		if (match(LEFT_PAREN)) {
+			Expr expr = expression();
+			consume(RIGHT_PAREN, "Expect ')' after expression.");
+			return new Expr.Grouping(expr);
+		}
+	}
+
 	private boolean match(TokenType... types) {
 		for (TokenType type : types) {
 			if (check(type)) {
